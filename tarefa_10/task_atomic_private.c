05bf03277178cf01877a2d012879d7ac1b75c5c7
Caio2a7 <caiodanielfonseca@gmail.com>
2026-04-21 23:09:00 -0300

    Add task 10

---
+++ tarefa_10/task_atomic_private.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+#define CIRCLE_RADIUS 1
+#define NUMBER_TRIES 500000000
+
+_Bool is_in_radius(int radius, float x, float y){
+    if (((x*x) + (y*y)) <= (radius*radius)){
+        return 1;
+    } 
+    else{
+        return 0;
+    }
+}
+
+int pi_stochastic_approximation(int number_tries, int circle_radius){
+    int private_numbers_in_range = 0, public_numbers_in_range = 0;
+    #pragma omp parallel num_threads(10) firstprivate(private_numbers_in_range)
+    {
+        unsigned int seed = (unsigned int)time(NULL) + omp_get_thread_num();
+        #pragma omp for
+        for(int i = 0; i < number_tries; i++){
+            float x = ((float)rand_r(&seed) / (float)RAND_MAX) * circle_radius;
+            float y = ((float)rand_r(&seed) / (float)RAND_MAX) * circle_radius;
+            if(is_in_radius(circle_radius, x, y) == 1){
+                private_numbers_in_range++;
+            }
+        }
+        printf("TID: %d - Numbers in Range: %d\n", omp_get_thread_num(), private_numbers_in_range);
+        #pragma omp atomic
+        public_numbers_in_range += private_numbers_in_range;
+    }
+    return public_numbers_in_range;
+}
+
+int main(){
+    int numbers_in_range = pi_stochastic_approximation(NUMBER_TRIES, CIRCLE_RADIUS);
+    double pi = ((double)numbers_in_range / (double)NUMBER_TRIES) * 4;
+    printf("\nAproximação estocástica de pi: %.10lf\n", pi);
+    return 0;
+}
