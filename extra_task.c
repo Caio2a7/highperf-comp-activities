b4e03426b58283962c8e8c645bb73c5bf511a25c
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2026-05-13 15:46:11 -0300

    Update extra_task.c

--- extra_task.c
+++ extra_task.c
@@ -2,18 +2,19 @@
 #include <math.h>
 #include <omp.h>
 
-#define DOTS_NUM 10
+#define DOTS_NUM 1000
+#define dot_b 10
+#define dot_a 1
 
 typedef double (*target_function)(double);
 
 void integral_aproximmation(target_function fn){
     double sum = 0;
-    for(size_t i = 1; i < DOTS_NUM-1; i++){
+    for(size_t i = 1; i <= DOTS_NUM-1; i++){
         sum += fn(i);
     }
     double dx = DOTS_NUM;
     double h = (dx - 1) / dx;
-    printf("H: %.5lf\n", (dx - 1));
     double integral = h * ((fn(1)/2) + (fn(dx)/2) + sum);
     printf("Integral: %.5lf\n", integral);
 } 
