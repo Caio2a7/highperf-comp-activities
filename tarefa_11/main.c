cf9a296cf5cd4071bf7601c5ec655e2df59a8a7b
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2026-05-25 18:13:53 -0300

    Refactor update_velocity function parameters and logic

--- tarefa_11/main.c
+++ tarefa_11/main.c
@@ -66,21 +66,22 @@ float advection_in_y(float first_term, float second_term, float third_term, int 
     return ((powf(first_term, n)) * (powf(second_term, n) - powf(third_term, n))) / (dy*2);
 }
 
-void update_velocity(int n, float dx, float dy, float dt, int nx, int ny, float viscosity){
-    printf("DX: %0.5lf - DY: %0.5lf \n", dt, dy);
+void update_velocity(float dx, float dy, float dt, int nx, int ny, float viscosity){
+    int n = 0;
+            printf("N: %d\n", n);
     for(int i = 1; i <= nx-2; i++){
         for(int j = 1; j <= ny-2; j++){
+            printf("N: %d\n", n);
             u[i][j] = (powf(u[i][j], n)) + dt * (viscosity * (diffusion_in_x(u[i+1][j], u[i][j], u[i-1][j], n, dx) + diffusion_in_y(u[i][j+1], u[i][j], u[i][j-1], n, dy)) - advection_in_y(v[i][j], u[i][j+1], u[i][j-1], n, dy) - advection_in_x(u[i][j], u[i+1][j], u[i-1][j], dx, n));
-            n += 1;
         }
+        n +=  1;
     }
 }
 
 int main(){
     cfl_compute_time_spacing(DX, DY, VISCOSITY);
     initialize_grid(GRID_POINTS_X, GRID_POINTS_Y);
-    printf("DX: %0.5lf - DY: %0.5lf - TIME_SPACE: %0.5lf\n", DX, DY, time_spacing);
 
-    update_velocity(time_step, DX, DY, time_spacing, GRID_POINTS_X, GRID_POINTS_Y, VISCOSITY);
+    update_velocity(DX, DY, time_spacing, GRID_POINTS_X, GRID_POINTS_Y, VISCOSITY);
     print_grid(GRID_POINTS_X, GRID_POINTS_Y, u);
 }
